a09daf88ef1f0de26727426043186cc453e5b31d
marelas <marela.as@gmail.com>
2022-06-17 17:57:29 -0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
-import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeroesComponent,
-    ProductsComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
