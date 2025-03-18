83077ce026e6865ee2944acc32c54e467cf6a82f
marelas <marela.as@gmail.com>
2025-03-17 23:46:58 -0300

    fix main page structure

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+  oidc: {
+    clientId: '0oa6051ypbNXHoXek5d7',
+    issuer: 'https://dev-61858018.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:4200/login/callback',
+    scopes: ['openId', 'profile', 'email']
+  }
+
+}
